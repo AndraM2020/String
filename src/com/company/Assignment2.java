6384011a2bb73e611ae523e0d83212ed56bd2ce7
AndraM2020 <andramozga@gmail.com>
2021-10-22 16:16:12 +0300

    Improvements

--- src/com/company/Assignment2.java
+++ src/com/company/Assignment2.java
@@ -2,34 +2,55 @@ package com.company;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        String theMountainThing = "Climb mountains not so the world can see you, but so you can see the world";
+        String theMountainSentence = "Climb mountains not so the world can see you, but so you can see the world";
+        String youToUpperCase = theMountainSentence.replaceAll("you", "You");
+        System.out.println(youToUpperCase);
 
-        //how many words in the sentence
-        String[] wordsArr = theMountainThing.replaceAll("[^a-z A-Z]", "").toLowerCase().split("\\s+");//https://stackoverflow.com/questions/18830813/how-can-i-remove-punctuation-from-input-text-in-java
+        wordsInSentece(theMountainSentence);
+        theInSentence(theMountainSentence);
+        wordsWithS(theMountainSentence);
+        repeatedWords(theMountainSentence);
+    }
+
+    public static void wordsInSentece(String theMountainSentence) {
+        String[] wordsArr = theMountainSentence.replaceAll("[^a-z A-Z]", "").toLowerCase().split("\\s+");//https://stackoverflow.com/questions/18830813/how-can-i-remove-punctuation-from-input-text-in-java
         System.out.println("There are " + wordsArr.length + " words in the sentence.");
+    }
 
-        //how many "the" in the sentence
+    public static void theInSentence(String theMountainSentence) {
+        String[] wordsArr = theMountainSentence.split("\\s+");
         int count = 0;
-        for (int i = 0; i < wordsArr.length; i++) {
+        for (
+                int i = 0;
+                i < wordsArr.length; i++) {
             if (wordsArr[i].equalsIgnoreCase("the")) {
                 count++;
             }
         }
         System.out.println("There are " + count + " 'the' in the sentence.");
+    }
 
-        //how many words contain letter "C"
+    public static void wordsWithS(String theMountainSentence) {
+        String[] wordsArr = theMountainSentence.split("\\s+");
         int count1 = 0;
-        for (int i = 0; i < wordsArr.length; i++) {
+        for (
+                int i = 0;
+                i < wordsArr.length; i++) {
             if (wordsArr[i].contains("s")) {
                 count1++;
             }
         }
         System.out.println("There are " + count1 + " words containing letter 's'.");
+    }
 
-        //words that are repeated
+
+    public static void repeatedWords(String theMountainSentence) {
+        String[] wordsArr = theMountainSentence.split("\\s+");
         System.out.print("These words are repeated in the sentence: ");
         int count2 = 0;
-        for (int i = 0; i < wordsArr.length; i++) {
+        for (
+                int i = 0;
+                i < wordsArr.length; i++) {
             count2 = 1;
             for (int j = i + 1; j < wordsArr.length; j++) {
                 if (wordsArr[i].equalsIgnoreCase(wordsArr[j])) {
@@ -39,14 +60,9 @@ public class Assignment2 {
             }
             if (count2 > 1 && wordsArr[i] != "0") {
                 System.out.print(wordsArr[i] + "; ");
-
             }
         }
         System.out.println("");
-
-        //changing you to You
-        String youToUpperCase = theMountainThing.replaceAll("you", "You");
-        System.out.println(youToUpperCase);
-
     }
 }
+
